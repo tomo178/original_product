57397426cf9d7597404938a22e01d381935b9597
tomo178 <inaba.dangan@gmail.com>
2023-07-12 18:08:24 +0900

    rooting

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -3,6 +3,6 @@
 //= link_tree ../../javascript .js
 //= link_tree ../../../vendor/javascript .js
 
-//= link products/products.css
-//= link home/home.css
-//= link posts/posts.css
+//= link products/products.scss
+//= link home/home.scss
+//= link posts/posts.scss
