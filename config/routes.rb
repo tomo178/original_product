99db2295213a1601b80cb4d18f2e8d7cc3fbff95
tomo178 <inaba.dangan@gmail.com>
2023-07-13 14:35:36 +0900

    routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
   get 'posts/edit'
-  get 'products/index'
-  get 'home/index'
+  get 'products', to: 'products#index'
+  get 'home', to: 'home#index'
   get 'base/authorize', to: 'base#authorize'
   get 'base/get_items', to: 'base#get_items'
+  root 'home#index'
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
